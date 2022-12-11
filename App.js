b4249690f40dbaf86247545b51b7d9579982df04
RohanRusta21 <Rohanrustagi21@gmail.com>
2022-12-11 16:18:05 +0530

    first commit

--- App.js
+++ App.js
@@ -9,6 +9,15 @@ import 'firebase/firestore'
 
 const firebaseConfig = {
     //Your firebase config here
+    // Initialize Firebase
+    apiKey: "AIzaSyDFRBH7rn_b2OvKRetNXlM58C0mz4p7KJY",
+    authDomain: "react-chat-90955.firebaseapp.com",
+    databaseURL: "https://react-chat-90955-default-rtdb.firebaseio.com",
+    projectId: "react-chat-90955",
+    storageBucket: "react-chat-90955.appspot.com",
+    messagingSenderId: "497984503239",
+    appId: "1:497984503239:web:9684fe5f0a021492516de2"
+
 }
 
 if (firebase.apps.length === 0) {
